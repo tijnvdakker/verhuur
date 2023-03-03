157546124f82752c2494c8c2035db12d264df9f9
tijnvdakker <tvandenakker2005@gmail.com>
2023-03-03 13:09:09 +0100

    Aanpassingen voor les 3-3

--- server/index.js
+++ server/index.js
@@ -13,9 +13,12 @@ app.get('/api/products', async (req, res) => {
     let today = dateStringHelper.today();
 
     let products = await db('products as p')
-        .select('p.*', db.raw('COALESCE(SUM(s.mutation), 0) as total_stock'))
+        .select('p.*', db.raw('COALESCE(pg.color, "#cccccc") as color'), db.raw('COALESCE(SUM(s.mutation), 0) as current_stock'), db.raw('COALESCE(SUM(ss.mutation), 0) as total_stock'))
+        .leftJoin('product_groups as pg', 'p.product_group_id', 'pg.product_group_id')
         .leftJoin(
             db.raw(`stock as s ON s.product_id = p.product_id AND '${today}' >= COALESCE(s.date_start, '0000-00-00') AND '${today}' < COALESCE(s.date_end, '9999-99-99')`))
+        .leftJoin(
+            db.raw(`stock as ss ON ss.product_id = p.product_id AND ss.agreement_id IS NULL`))
         .groupBy('p.product_id');
 
     res.json({ products });
@@ -50,6 +53,45 @@ app.post('/api/product/update_price', async (req, res) => {
     let product_id = req.body.product_id;
 
     await db('products').update({ price }).where({ product_id });
+
+    res.sendStatus(200);
+});
+
+app.post('/api/product/update_linked_product_group', async (req, res) => {
+    let product_group_id = req.body.product_group_id;
+    let product_id = req.body.product_id;
+
+    await db('products').update({ product_group_id }).where({ product_id });
+
+    res.sendStatus(200);
+});
+
+app.get('/api/product/groups', async (req, res) => {
+    let productGroups = await db('product_groups').select('*');
+
+    res.json({productGroups})
+});
+
+app.post('/api/product/group/add', async (req, res) => {
+    await db('product_groups').insert({ name: "Nieuw" });
+
+    res.sendStatus(200);
+});
+
+app.post('/api/product/group/update_name', async (req, res) => {
+    let name = req.body.name;
+    let product_group_id = req.body.product_group_id;
+
+    await db('product_groups').update({ name }).where({ product_group_id });
+
+    res.sendStatus(200);
+});
+
+app.post('/api/product/group/update_color', async (req, res) => {
+    let color = req.body.color;
+    let product_group_id = req.body.product_group_id;
+
+    await db('product_groups').update({ color }).where({ product_group_id });
 
     res.sendStatus(200);
 });
@@ -100,10 +142,17 @@ app.post('/api/agreement/add_product', async (req, res) => {
     res.sendStatus(200);
 });
 
-app.get('/api/stock/total', async (req, res) => {
+app.get('/api/stock/total/:product_group_id', async (req, res) => {
     let today = dateStringHelper.today();
 
     let todayMinusDays = dateStringHelper.today(-90);
+
+    let product_group_id = req.params.product_group_id;
+
+    //Geen filter applyen
+    if (product_group_id == 0) {
+        product_group_id = "%%";
+    }
 
     let stockData = await db.raw(`WITH RECURSIVE dates AS (
                                     SELECT '${todayMinusDays}' AS _day_ 
@@ -114,7 +163,7 @@ app.get('/api/stock/total', async (req, res) => {
                                 ) 
                                 
                                 SELECT DATE_FORMAT(d._day_, "%Y-%m-%d") AS stock_date, 
-                                (SELECT COALESCE(SUM(s.mutation), 0) FROM stock AS s WHERE d._day_ >= COALESCE(s.date_start, '0000-00-00') AND d._day_ < COALESCE(s.date_end, '9999-99-99')) AS total_stock 
+                                (SELECT COALESCE(SUM(s.mutation), 0) FROM stock AS s JOIN products as p ON s.product_id = p.product_id WHERE d._day_ >= COALESCE(s.date_start, '0000-00-00') AND d._day_ < COALESCE(s.date_end, '9999-99-99') AND p.product_group_id LIKE '${product_group_id}') AS total_stock 
                                 FROM dates AS d`);
 
     //Raw KNEX query, the first index is the result of the query
@@ -183,6 +232,14 @@ app.post('/api/product/delete', async (req, res) => {
     let product_id = req.body.product_id;
 
     await db('products').where({ product_id }).del();
+
+    res.sendStatus(200);
+});
+
+app.post('/api/product/group/delete', async (req, res) => {
+    let product_group_id = req.body.product_group_id;
+
+    await db('product_groups').where({ product_group_id }).del();
 
     res.sendStatus(200);
 });
